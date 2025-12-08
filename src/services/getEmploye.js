f042008d8b4cbcf1c1c1dbf6034bc2db8c374a67
Volkan Akyel <volkan@uikielle.com>
2025-12-08 08:20:32 +0100

    add service file

--- src/services/getEmploye.js
+++ src/services/getEmploye.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 export const getEmploye = async () => {
   try {
-    const response = await axios.get('https://randomuser.me/api/?results=10');
-    return response?.data;
+    const response = await axios.get('https://randomuser.me/api/?results=10')
+    return response?.data
   } catch (err) {
-    throw new Error(err)
+    console.error('Failed to fetch employees:', err)
+    throw err
   }
-};
+}
